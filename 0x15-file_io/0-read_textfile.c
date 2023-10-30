dc8a7bc18a3df3057cce1a8356f34d70536bbb7a
Owusu Ansah Joy <owusuansahjoy@gmail.com>
2023-10-30 11:22:10 -0700

    Completed  0x15. C - File I/O project

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,39 @@
 #include "main.h"
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <stdlib.h>
+
 /**
- * read_textfile - This reads a text file and prints it to the POSTX stdo
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
  *
- * @filename: name of the file to read
- *
- * @letters: number of letters it should read and print
- *
- * Return: actual number of letters it could read and print
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
  */
-ssize_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_d;
-	ssize_t lenr, lenw;
+	ssize_t o, r, w;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
-	file_d = open(filename, O_RDONLY);
-	if (file_d == -1)
-		return (0);
-	buffer == malloc(sizeof(char) - letters);
+
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-	{
-		close(file_d);
 		return (0);
-	}
-	lenr = read(file_d, buffer, letters);
-	close(file_d);
-	if (lenr == -1)
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
 		return (0);
 	}
-	lenw = write(STDOUT_FILEND, buffer, lenr);
+
 	free(buffer);
-	if (lenr != lenw)
-		return (0);
-	return (lenw);
+	close(o);
+
+	return (w);
 }
